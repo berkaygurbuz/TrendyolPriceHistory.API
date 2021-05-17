9e0b64f2522055dec34a405bce54560fa4c5f587
Berkay Gürbüz <berkay-gurbuz@hotmail.com>
2021-05-17 12:21:30 +0300

    Initial commit

---
+++ PriceHistory.DataAcces/PriceHistoryDbContext.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PriceHistory.DataAcces
+{
+    public class PriceHistoryDbContext
+    {
+    }
+}
---
+++ PriceHistory.Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PriceHistory.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public float price { get; set; }
+    }
+}
